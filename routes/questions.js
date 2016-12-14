c9bc63434b40addfca5dd03e66620756b2fd08b5
Giovanni De Toni <giovanni.det@gmail.com>
2016-12-14 11:29:52 +0100

    Add new update route and correct database file

--- routes/questions.js
+++ routes/questions.js
@@ -28,12 +28,52 @@ router.get('/:id', function(req, res, next) {
             if (result == null || result == undefined) {
                 next('route');
             } else {
+                console.log(result.rating);
                  res.render('question', { title: 'UnInforma | '+result.title, 
-                                    cssfile: 'question.css', 
+                                    cssfile: 'question.css',
+                                    id : result.id,
                                     question: result.title,
                                     answer: result.answer,
+                                    rating: result.rating,
                                     tags: result.tags
                                    });  
+            }
+        }); 
+    }
+});
+
+router.post('/:id/update', function(req, res, next){
+    var id = parseInt(req.params["id"]);
+    if (id === null || isNaN(id)) {
+        next('route');
+    } else {
+        questionDAO.read(id, function(err, result) {
+            if (result == null || result == undefined) {
+                next('route');
+            } else {
+                
+                if (!isNaN(parseInt(req.body["up"]))) {
+                     result.rating['up'] = parseInt(result.rating["up"]) + parseInt(req.body["up"]);   
+                }
+                if (!isNaN(parseInt(req.body["down"]))) {
+                    result.rating['down'] = parseInt(result.rating["down"]) + parseInt(req.body["down"]);   
+                }
+                
+                questionDAO.update(id, result, function(err, result2) {
+                    if (result2 == null || result2 == undefined) {
+                        next('route');
+                    } else {
+                        res.render('question', { title: 'UnInforma | '+result.title, 
+                                    cssfile: 'question.css',
+                                    id: result.id,
+                                    question: result.title,
+                                    answer: result.answer,
+                                    rating: result.rating,
+                                    tags: result.tags,
+                                    voted: true
+                                   });  
+                    }
+                });     
             }
         }); 
     }
